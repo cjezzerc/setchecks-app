b2ed17b74a53138dd4009bad1596cb9fcdd271f2
Jeremy Craven <cjeremycraven@gmail.com>
2023-09-15 10:44:20 +0100

    port back to 5000 from 5001

--- VSMT_SETCHKS_APP/run_app.py
+++ VSMT_SETCHKS_APP/run_app.py
@@ -7,4 +7,4 @@ debug=True
 if len(sys.argv)>1:
     debug=sys.argv[1]
 
-setchks_app.create_app.create_app().run(debug=debug, host='0.0.0.0', port=5001)
+setchks_app.create_app.create_app().run(debug=debug, host='0.0.0.0', port=5000)
