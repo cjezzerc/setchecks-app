48749b3f2a4cb85089f11a05e59a79afd7609a3f
Jeremy Craven <cjeremycraven@gmail.com>
2023-10-18 13:36:50 +0100

    add iam bits

--- VSMT_SETCHKS_APP/deployment/stacks/service_account/iam.tf
+++ VSMT_SETCHKS_APP/deployment/stacks/service_account/iam.tf
@@ -39,7 +39,34 @@ data "aws_iam_policy_document" "iam_host_role_policy_document" {
       "arn:aws:secretsmanager:${var.aws_region}:${local.aws_account_id}:secret:${var.service_prefix}*"
     ]
   }
+  statement {
+    actions = [
+        "rds:Describe*",
+        "rds:List*"
+        ]
+    resources =  flatten([
+      [
+        "arn:aws:rds:${var.aws_region}:${local.aws_account_id}:*:${service_prefix}*",
+        "arn:aws:rds:${var.aws_region}:${local.aws_account_id}:db-proxy-endpoint:*",
+        "arn:aws:rds:${var.aws_region}:${local.aws_account_id}:db-proxy:*",
+        "arn:aws:rds:${var.aws_region}:${local.aws_account_id}:target-group:*"
+      ]
+    ])
+  }
+  statement {
+    actions = ["elasticache:*"]
+    resources = flatten([
+      [
+        "arn:aws:elasticache:${var.aws_region}:${local.aws_account_id}:${service_prefix}*",
+        "arn:aws:elasticache:${var.aws_region}:${local.aws_account_id}:replicationgroup:${service_prefix}*",
+        "arn:aws:elasticache:${var.aws_region}:${local.aws_account_id}:parametergroup:${service_prefix}*",
+        "arn:aws:elasticache:${var.aws_region}:${local.aws_account_id}:subnetgroup:${service_prefix}*",
+        "arn:aws:elasticache:${var.aws_region}:${local.aws_account_id}:snapshot:${service_prefix}*"
+      ]
+    ])
+  }
 }
+
 
 resource "aws_iam_role_policy" "iam_host_role_policy" {
   role   = aws_iam_role.iam_host_role.id
