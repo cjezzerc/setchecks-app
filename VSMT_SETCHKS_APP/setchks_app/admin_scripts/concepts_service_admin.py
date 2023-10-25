969e2e3a9992e2b3ab357797ad15200aea81196a
Jeremy Craven <cjeremycraven@gmail.com>
2023-10-25 22:08:44 +0100

    add secrets get to concepts admin script; try increase mem  limit to 8G; add vi to Dockerfile

--- VSMT_SETCHKS_APP/setchks_app/admin_scripts/concepts_service_admin.py
+++ VSMT_SETCHKS_APP/setchks_app/admin_scripts/concepts_service_admin.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
-import sys, os
+import sys, os, json
 
-import logging
-logging.basicConfig(
-    format="%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s >>> %(message)s",
-    datefmt="%Y-%m-%dT%H:%M:%SZ",
-    level=logging.DEBUG,
-)
-logger=logging.getLogger(__name__)
+import boto3
 
 sys.path.append('../..')
 
 from setchks_app.concepts_service.concepts_service import ConceptsService
+
+print("About to see if need to get secrets")
+if 'ONTOSERVER_USERNAME' not in os.environ:
+    print("getting secrets")
+    sm_client = boto3.client('secretsmanager', region_name='eu-west-2')
+    pw_response = sm_client.get_secret_value(SecretId='vsmt-ontoserver-access')
+    passwords = pw_response['SecretString']
+    dictionary_pw = json.loads(passwords)
+    os.environ['ONTOSERVER_USERNAME']=dictionary_pw['ONTOSERVER_USERNAME']
+    os.environ['ONTOSERVER_SECRET']=dictionary_pw['ONTOSERVER_SECRET']
+    os.environ['TRUDAPIKEY']=dictionary_pw['TRUDAPIKEY']
+    os.environ['DOCUMENTDB_USERNAME']=dictionary_pw['DOCUMENTDB_USERNAME']
+    os.environ['DOCUMENTDB_PASSWORD']=dictionary_pw['DOCUMENTDB_PASSWORD']
+    print("got secrets")
+else:
+    print("no need to get secrets")
 
 cs=ConceptsService()
 
