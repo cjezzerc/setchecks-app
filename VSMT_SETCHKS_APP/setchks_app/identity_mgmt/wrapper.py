06b1589fb4d4f7f4553e1db5150125c6dfde8d67
Jeremy Craven <cjeremycraven@gmail.com>
2025-03-06 15:18:12 +0000

    add trial CLOSED message to test env only for regular users

--- VSMT_SETCHKS_APP/setchks_app/identity_mgmt/wrapper.py
+++ VSMT_SETCHKS_APP/setchks_app/identity_mgmt/wrapper.py
@@ -14,13 +14,11 @@ from setchks_app.identity_mgmt.get_token import get_token_from_refresh_token
 def auth_required(f):
     @functools.wraps(f) # not quite sure why added this but now if remove it get overwriting existing endpoint error
     def wrap2(*args, **kwargs):
-        # print(list(session.keys()))
         authorized=False
         have_token='jwt_token' in session.keys()
         if have_token:
             jwt_token=session['jwt_token']
             print(jwt_token)
-            # print(jwt_token)
             if time.time()<jwt_token['exp']:
                 authorized=True
             else:
@@ -34,16 +32,23 @@ def auth_required(f):
                 cognito_group_memberships={}
             level_allowed=True 
             if os.environ['DEPLOYMENT_ENV'] == "AWS":
-                # if os.environ['DEPLOYMENT_AWSENV'] !="demo": # only let vsmt_admin into environments other than demo
-                if os.environ['ENV'] !="demo": # only let vsmt_admin into environments other than demo
+                if os.environ['ENV'] !="demo": # only let vsmt_internal into environments other than demo
                     level_allowed="vsmt_internal" in cognito_group_memberships
+            
+            
+            ####################################################################
+            # put up closed message in DEMO environment for non internal users #
+            ####################################################################
+            if os.environ['ENV'] == "test":  # change this to demo to close down demo
+              if "vsmt_internal" not in cognito_group_memberships:
+                return "The Set Checks (Proof of Concept) is no longer available"
+
             if level_allowed:
                 return f(*args, **kwargs)
             else:
                 return "You are not authorised to access this endpoint in this environment"    
         
         else:
-            # session['function_provoking_auth_call']=url_for('setchks_app.'+f.__name__) # trying dropping this to simplify things
             session['function_provoking_auth_call']='/data_upload'
             return redirect(url_for('setchks_app.cognito_test'))
     return wrap2
