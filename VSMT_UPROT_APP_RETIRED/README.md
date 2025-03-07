8598b6c09f4843fee36ccba7c3b5615a4d947867
Jeremy Craven <cjeremycraven@gmail.com>
2025-03-07 15:06:12 +0000

    delete old VSMT_UPROT_APP_RETIRED code

--- VSMT_UPROT_APP_RETIRED/README.md
+++
@@ -1,3 +0,0 @@
-# Value-Set
-
-This is a very early micro-prototyping codebase for the Value Set Management Tool project during the discovery phase. 
--- VSMT_UPROT_APP_RETIRED/run_pip.bat
+++
@@ -1,4 +0,0 @@
-
-REM THis is a convenience script while checking installations running under pure Windows
-
-C:%HOMEPATH%\AppData\Local\Programs\Python\Python%1\scripts\pip install -r requirements.txt
