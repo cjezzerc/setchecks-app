a573711fbae1bb71c3428b95edc817afba7d1941
Jeremy Craven <cjeremycraven@gmail.com>
2025-04-16 15:55:16 +0100

    remove logging and actually use

--- VSMT_SETCHKS_APP/setchks_app/mongodb/get_mongodb_client.py
+++ VSMT_SETCHKS_APP/setchks_app/mongodb/get_mongodb_client.py
@@ -59,6 +59,5 @@ def get_mongodb_client():
                     credentials=f"{os.environ['MONGODB_USER']}:{os.environ['MONGODB_PASSWORD']}@"
                 else:
                     credentials=""
-                logger.debug(f"connect string: mongodb://{credentials}127.0.0.1:27017")
-                mongodb_client=MongoClient(f"")
+                mongodb_client=MongoClient(f"mongodb://{credentials}127.0.0.1:27017")
     return mongodb_client
