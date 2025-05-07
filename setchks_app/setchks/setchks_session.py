cjezzerc/setchecks-app

commit 05facfdd66f7268f9f830357692ccd39951aa073

amend way that available sct versions stored; fix how hidden arrow button drawn

setchks_app/setchks/setchks_session.py
@@ -2,6 +2,8 @@
 
 import uuid
 
+from flask import current_app
+
 from ..excel import generate_excel_output
 from setchks_app.data_as_matrix import load_data_into_matrix
 from setchks_app.data_as_matrix import column_content_assessment
@@ -124,7 +126,7 @@ class SetchksSession():
         self.timings={} # arbitrary entries can be put in this dict for debug purposes
         self.app_version="NOT SET" # will stay as "NOT SET" if not running from app (i.e. via script)
         self.environment="NOT SET" # ditto
-        self.initialise_sct_versions()
+        self.set_sct_versions()
         
     def __repr__(self):
         repr_strings=[]
@@ -175,15 +177,15 @@ class SetchksSession():
         self.all_CHKXX_finished=False
         self.processing_status="1_CHECKS_READY_TO_RUN" # strictly only the case if data loaded, sct_releases chosen etc
 
-    def initialise_sct_versions(self):
-        all_available_sct_versions={x.date_string: x for x in get_sct_versions.get_sct_versions()}
-        self.available_sct_versions=[]
-        ds=DescriptionsService(data_type="hst")
-        hst_dict=ds.check_whether_releases_on_ontoserver_have_collections()
-        for sct_version, hst_exists in hst_dict.items():
-            if hst_exists: # only make sct_version available if has an HST 
-                self.available_sct_versions.append(all_available_sct_versions[sct_version])
-
+    def set_sct_versions(self):
+        # all_available_sct_versions={x.date_string: x for x in get_sct_versions.get_sct_versions_on_ontoserver()}
+        # self.available_sct_versions=[]
+        # ds=DescriptionsService(data_type="hst")
+        # hst_dict=ds.check_whether_releases_on_ontoserver_have_collections()
+        # for sct_version, hst_exists in hst_dict.items():
+        #     if hst_exists: # only make sct_version available if has an HST 
+        #         self.available_sct_versions.append(all_available_sct_versions[sct_version])
+        self.available_sct_versions=current_app.config["sct_versions_available_in_app"]
         self.sct_version=self.available_sct_versions[0]
         self.sct_version_b=self.available_sct_versions[0]
 

setchks_app/concepts_service/concepts_service.py
@@ -27,7 +27,7 @@ class ConceptsService():
         # self.db=MongoClient()["VSMT_uprot_app"]
     
     def get_list_of_releases_on_ontoserver(self):
-        return [x.date_string for x in get_sct_versions.get_sct_versions()]
+        return [x.date_string for x in get_sct_versions.get_sct_versions_on_ontoserver()]
     
     def check_have_sct_version_collection_in_db(self, sct_version=None):
         collection_name=self.make_collection_name(date_string=sct_version)

setchks_app/create_app.py
@@ -12,6 +12,8 @@ from setchks_app.redis.rq_utils import (
     kill_all_rq_workers,
     start_specific_rq_worker,
     )
+from setchks_app.sct_versions import get_sct_versions
+
 
 logging.basicConfig(
     format="%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s >>> %(message)s",
@@ -64,4 +66,6 @@ def create_app():
     else:
         app.config["ENVIRONMENT"]="LOCAL"
 
+    app.config["sct_versions_available_in_app"]=get_sct_versions.get_sct_versions_available_in_app()
+    
     return app

setchks_app/descriptions_service/descriptions_service.py
@@ -59,7 +59,7 @@ class DescriptionsService():
         return self.db[collection_name].count_documents({})
     
     def get_list_of_releases_on_ontoserver(self):
-        return [x.date_string for x in get_sct_versions.get_sct_versions()]
+        return [x.date_string for x in get_sct_versions.get_sct_versions_on_ontoserver()]
     
     def make_collection_name(self, date_string=None):
         if self.data_type=="descriptions":

setchks_app/endpoints/enter_metadata.py
@@ -15,7 +15,9 @@ from setchks_app.sct_versions import graphical_timeline
 @auth_required
 def enter_metadata():
     setchks_session=gui_setchks_session.get_setchk_session(session)
- 
+    setchks_session.set_sct_versions() # this is belt and braces for case where extra sct
+                                       # versions loaded suring a session
+    
     current_sct_version=setchks_session.sct_version # remember this in case changes in next sections
     current_sct_version_b=setchks_session.sct_version_b # remember this in case changes in next sections
 

setchks_app/sct_versions/get_sct_versions.py
@@ -1,30 +1,44 @@
 """ function to pull version list from terminology server and convert to date sorted list of SCT objects"""
 
 import os
+
 from flask import current_app
-from . import sct_version
 from fhir.resources.bundle import Bundle
+
+from . import sct_version
 import setchks_app.terminology_server_module
-from setchks_app.fhir_utils.fhir_utils import repr_resource
+from setchks_app.descriptions_service.descriptions_service import DescriptionsService
 
-def get_sct_versions():
 
-        terminology_server=setchks_app.terminology_server_module.TerminologyServer()
-        relative_url= "CodeSystem?url=http://snomed.info/sct"
-        response=terminology_server.do_get(relative_url=relative_url, verbose=True) 
-        bundle=Bundle.parse_obj(response.json())
+def get_sct_versions_on_ontoserver():
 
-        # for be in bundle.entry:
-        #     print("\n".join(repr_resource(resource=be.resource)))
+    terminology_server=setchks_app.terminology_server_module.TerminologyServer()
+    relative_url= "CodeSystem?url=http://snomed.info/sct"
+    response=terminology_server.do_get(relative_url=relative_url, verbose=True) 
+    bundle=Bundle.parse_obj(response.json())
 
-        available_sct_versions=[be.resource.dict()["version"] for be in bundle.entry]
-        available_sct_versions=[sct_version.SctVersion(formal_version_string=x) for x in available_sct_versions]
-        available_sct_versions.sort(key=get_sortable_date_part, reverse=True) # mild overkill since as it stands sorting on 
-                                                                                # whole formal_version_string would work
-        # TEMPORARY FIX FOR FAMILIARISATION DAY - REMOVE OCT VERSION AS HAS NOT HST AND SO MEANS THE DEFAULT CURRENT VERSION IS PROBLEMATIC
-        # available_sct_versions=available_sct_versions[1:]
-        return available_sct_versions
+    # for be in bundle.entry:
+    #     print("\n".join(repr_resource(resource=be.resource)))
+
+    available_sct_versions_on_ontoserver=[be.resource.dict()["version"] for be in bundle.entry]
+    available_sct_versions_on_ontoserver=[sct_version.SctVersion(formal_version_string=x) for x in available_sct_versions_on_ontoserver]
+    available_sct_versions_on_ontoserver.sort(key=get_sortable_date_part, reverse=True) # mild overkill since as it stands sorting on 
+                                                                            # whole formal_version_string would work
+    return available_sct_versions_on_ontoserver
 
 
 def get_sortable_date_part(sct_version):
     return sct_version.date_string
+
+def get_sct_versions_available_in_app():
+    all_available_sct_versions={x.date_string: x for x in get_sct_versions_on_ontoserver()}
+    available_sct_versions_in_app=[]
+    ds=DescriptionsService(data_type="hst")
+    hst_dict=ds.check_whether_releases_on_ontoserver_have_collections()
+    for sct_version, hst_exists in hst_dict.items():
+        if hst_exists: # only make sct_version available if has an HST 
+            available_sct_versions_in_app.append(all_available_sct_versions[sct_version])
+    return available_sct_versions_in_app
+
+
+                

setchks_app/static/css/setchks.css
@@ -51,6 +51,7 @@ body {
     margin: 0 0;
     -webkit-text-stroke: 2px;
     }
+
 .setchks-forw-back-btn:hover {
     background-color:white;
     /* background-color:#f7e203;  */
@@ -59,15 +60,29 @@ body {
     border-color:#005eb8; 
     -webkit-text-stroke: 3px;
     }
+
 .setchks-forw-back-btn-inactive {
     background-color: lightgray;
     border-color: grey;
     color: grey;
     }
+
 .setchks-forw-back-btn-inactive:hover {
     background-color: lightgray;
     color: lightslategray;
     -webkit-text-stroke: 2px;
+    }
+
+.setchks-forw-back-btn-hidden {
+    background-color: lightgray;
+    border-color: grey;
+    color: lightgray;
+    }
+    
+.setchks-forw-back-btn-hidden:hover {
+    background-color: lightgray;
+    border-color: grey;
+    color: lightgray;
     }
 
 .setchks-breadcrumb-strong {font-weight: bold; color:black}

setchks_app/templates/navigation.html
@@ -36,7 +36,7 @@
                     <i class="bi bi-arrow-left"></i> 
                 </a>
             {% else %}
-                <a href="{{ href }}" class="btn setchks-forw-back-btn setchks-forw-back-btn-inactive ">
+                <a href="{{ href }}" class="btn setchks-forw-back-btn setchks-forw-back-btn-hidden ">
                     <i class="bi bi-arrow-left"></i> 
                 </a>
             {% endif %}

commit 63d5c2c0a16328740a9fad79d12aa09510442113

fix bug introduced in 05facfdd66f7268f9f830357692ccd39951aa073 that made it reset sct_version every time visited enter_metadata

setchks_app/setchks/setchks_session.py
@@ -186,8 +186,9 @@ class SetchksSession():
         #     if hst_exists: # only make sct_version available if has an HST 
         #         self.available_sct_versions.append(all_available_sct_versions[sct_version])
         self.available_sct_versions=current_app.config["sct_versions_available_in_app"]
-        self.sct_version=self.available_sct_versions[0]
-        self.sct_version_b=self.available_sct_versions[0]
+        if self.sct_version is None:
+            self.sct_version=self.available_sct_versions[0]
+            self.sct_version_b=self.available_sct_versions[0]
 
     def do_SCT_release_dependent_preprocessing(self):
         self.passes_gatekeeper=True