78dbd980991f05dd8e4f953031278c10d5a778da
Jeremy Craven <cjeremycraven@gmail.com>
2025-06-11 10:37:33 +0100

    fix circular import created in 05facfdd66f7268f9f830357692ccd39951aa073

--- setchks_app/create_app.py
+++ setchks_app/create_app.py
@@ -11,8 +11,9 @@ from setchks_app.redis.get_redis_client import get_redis_string
 from setchks_app.redis.rq_utils import (
     kill_all_rq_workers,
     start_specific_rq_worker,
-    )
+)
 from setchks_app.sct_versions import get_sct_versions
+from setchks_app.descriptions_service.descriptions_service import DescriptionsService
 
 
 logging.basicConfig(
@@ -20,13 +21,14 @@ logging.basicConfig(
     datefmt="%Y-%m-%dT%H:%M:%SZ",
     level=logging.DEBUG,
 )
-logger=logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
+
 
 def create_app():
 
     app = Flask(__name__, instance_relative_config=True)
     app.register_blueprint(setchecks_app.bp)
-    
+
     oauth = OAuth(app)
     oauth.register(
         "auth0",
@@ -34,19 +36,21 @@ def create_app():
         client_secret=os.environ["AUTH0_CLIENT_SECRET"],
         client_kwargs={
             "scope": "openid profile email offline_access",
-            },
-        server_metadata_url=f'https://{os.environ["AUTH0_DOMAIN"]}/.well-known/openid-configuration'
-        )
-    app.config["oauth"]=oauth
+        },
+        server_metadata_url=f'https://{os.environ["AUTH0_DOMAIN"]}/.well-known/openid-configuration',
+    )
+    app.config["oauth"] = oauth
 
-    app.config['SESSION_TYPE'] = 'redis'
-    app.config['SESSION_PERMANENT'] = False
-    app.config['SESSION_USE_SIGNER'] = True
-    redis_connection=from_url(get_redis_string())
-    app.config['SESSION_REDIS'] = redis_connection
-    server_session = flask_session.Session(app) # resets session variable behaviour so uses redis
+    app.config["SESSION_TYPE"] = "redis"
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_USE_SIGNER"] = True
+    redis_connection = from_url(get_redis_string())
+    app.config["SESSION_REDIS"] = redis_connection
+    server_session = flask_session.Session(
+        app
+    )  # resets session variable behaviour so uses redis
 
-    app.secret_key = os.environ['FLASK_APP_SECRET']
+    app.secret_key = os.environ["FLASK_APP_SECRET"]
 
     kill_all_rq_workers()
     start_specific_rq_worker(worker_name="worker_long_jobs")
@@ -55,17 +59,24 @@ def create_app():
     # Get Git commit and "cleanness" NB Hot code changes will require restart
     # cleanness check not 100% reliable under WSL; sometimes seems sensitive to the line ending issue
     # and reports many files as modified though they are not. But seems erratic.
-    app.config["VERSION"]=os.popen('git log --format=%h --abbrev=8 -n 1').readlines()[0].strip()
-    git_status_output=os.popen('git status --short').readlines()
+    app.config["VERSION"] = (
+        os.popen("git log --format=%h --abbrev=8 -n 1").readlines()[0].strip()
+    )
+    git_status_output = os.popen("git status --short").readlines()
     logger.debug(f"git_status_output={str(git_status_output)}")
-    if  git_status_output!= []:
-        app.config["VERSION"]+="(modified)"
-        
-    if os.environ["DEPLOYMENT_ENV"]=="AWS": 
-        app.config["ENVIRONMENT"]=os.environ["ENV"].upper() # just used in Jinja template
-    else:
-        app.config["ENVIRONMENT"]="LOCAL"
+    if git_status_output != []:
+        app.config["VERSION"] += "(modified)"
 
-    app.config["sct_versions_available_in_app"]=get_sct_versions.get_sct_versions_available_in_app()
-    
+    if os.environ["DEPLOYMENT_ENV"] == "AWS":
+        app.config["ENVIRONMENT"] = os.environ[
+            "ENV"
+        ].upper()  # just used in Jinja template
+    else:
+        app.config["ENVIRONMENT"] = "LOCAL"
+
+    ds = DescriptionsService(data_type="hst")
+    app.config["sct_versions_available_in_app"] = (
+        get_sct_versions.get_sct_versions_available_in_app(descriptions_service=ds)
+    )
+
     return app
--- setchks_app/sct_versions/get_sct_versions.py
+++ setchks_app/sct_versions/get_sct_versions.py
@@ -1,4 +1,4 @@
-""" function to pull version list from terminology server and convert to date sorted list of SCT objects"""
+"""function to pull version list from terminology server and convert to date sorted list of SCT objects"""
 
 import os
 
@@ -7,38 +7,47 @@ from fhir.resources.bundle import Bundle
 
 from . import sct_version
 import setchks_app.terminology_server_module
-from setchks_app.descriptions_service.descriptions_service import DescriptionsService
 
 
 def get_sct_versions_on_ontoserver():
 
-    terminology_server=setchks_app.terminology_server_module.TerminologyServer()
-    relative_url= "CodeSystem?url=http://snomed.info/sct"
-    response=terminology_server.do_get(relative_url=relative_url, verbose=True) 
-    bundle=Bundle.parse_obj(response.json())
+    terminology_server = setchks_app.terminology_server_module.TerminologyServer()
+    relative_url = "CodeSystem?url=http://snomed.info/sct"
+    response = terminology_server.do_get(relative_url=relative_url, verbose=True)
+    bundle = Bundle.parse_obj(response.json())
 
     # for be in bundle.entry:
     #     print("\n".join(repr_resource(resource=be.resource)))
 
-    available_sct_versions_on_ontoserver=[be.resource.dict()["version"] for be in bundle.entry]
-    available_sct_versions_on_ontoserver=[sct_version.SctVersion(formal_version_string=x) for x in available_sct_versions_on_ontoserver]
-    available_sct_versions_on_ontoserver.sort(key=get_sortable_date_part, reverse=True) # mild overkill since as it stands sorting on 
-                                                                            # whole formal_version_string would work
+    available_sct_versions_on_ontoserver = [
+        be.resource.dict()["version"] for be in bundle.entry
+    ]
+    available_sct_versions_on_ontoserver = [
+        sct_version.SctVersion(formal_version_string=x)
+        for x in available_sct_versions_on_ontoserver
+    ]
+    available_sct_versions_on_ontoserver.sort(
+        key=get_sortable_date_part, reverse=True
+    )  # mild overkill since as it stands sorting on
+    # whole formal_version_string would work
     return available_sct_versions_on_ontoserver
 
 
 def get_sortable_date_part(sct_version):
     return sct_version.date_string
 
-def get_sct_versions_available_in_app():
-    all_available_sct_versions={x.date_string: x for x in get_sct_versions_on_ontoserver()}
-    available_sct_versions_in_app=[]
-    ds=DescriptionsService(data_type="hst")
-    hst_dict=ds.check_whether_releases_on_ontoserver_have_collections()
+
+def get_sct_versions_available_in_app(descriptions_service=None):
+    all_available_sct_versions = {
+        x.date_string: x for x in get_sct_versions_on_ontoserver()
+    }
+    available_sct_versions_in_app = []
+    hst_dict = (
+        descriptions_service.check_whether_releases_on_ontoserver_have_collections()
+    )
     for sct_version, hst_exists in hst_dict.items():
-        if hst_exists: # only make sct_version available if has an HST 
-            available_sct_versions_in_app.append(all_available_sct_versions[sct_version])
+        if hst_exists:  # only make sct_version available if has an HST
+            available_sct_versions_in_app.append(
+                all_available_sct_versions[sct_version]
+            )
     return available_sct_versions_in_app
-
-
-                
